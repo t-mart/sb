81f000a379e729248a14a8fabd19fbfe9f599515
Tim Martin <tim@timmart.in>
2025-11-08 22:54:49 -0600

    recheck

--- src/sb/__main__.py
+++ src/sb/__main__.py
@@ -190,6 +190,43 @@ def ls(client: str, status: TorrentStatusesT | None):
             click.echo(f"{torrent.name} ({torrent.hash})", err=True)
 
 
+@sb.command()
+@click.option(
+    "--client",
+    "-c",
+    multiple=True,
+    help="Specify which clients to add to",
+    required=True,
+)
+@click.option(
+    "--status",
+    "-s",
+    type=click.Choice(get_args(TorrentStatusesT)),
+    default=None,
+    help="Filter torrents by status",
+)
+def recheck(client: tuple[str], status: TorrentStatusesT | None):
+    """Recheck all torrents in specified CLIENT(s)."""
+    config = Config.load_from_file()
+
+    for client_name in client:
+        client_config = get_client_config(config, client_name)
+
+        with QBittorrentClient(
+            host=client_config.url,
+            username=client_config.username,
+            password=client_config.password,
+            category=client_config.category,
+        ) as qb_client:
+            click.echo(f"Client '{client_name}'", err=True)
+
+            torrents = qb_client.list_torrents(status=status)
+
+            for torrent in torrents:
+                click.echo(f"\tRechecking torrent {torrent.name}", err=True)
+                qb_client.start_recheck(torrent.hash)
+
+
 def get_client_config(config: Config, client_name: str):
     try:
         return config.clients[client_name]
